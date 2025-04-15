1184ba3ed9750df57480d52b2081ccd02c3f770b
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-04-15 00:00:51 -0500

    Integración de ejercicios de bucles

--- REPASO/src/main/java/org/example/Main.java
+++ REPASO/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("============================================================");
+            System.out.println("Hola bienvenido al sistema:");
+        System.out.println("============================================================");
+        System.out.println("");
+        System.out.print("Ingrese el número limite a repetir: ");
+            Scanner keyboard = new Scanner(System.in);
+            int limitNumber = keyboard.nextInt();
+
+            int centinela=0;
+
+            while(centinela<=limitNumber){
+
+                System.out.println("El número es: " + centinela);
+
+                centinela++;
+            }
+        System.out.println("FIN DEL PROGRAMA VUELVA PRONTO, NÚMERO LIMITE FUE DE:  " + limitNumber);
+
     }
 }
